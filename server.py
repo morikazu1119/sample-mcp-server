c66c47011bfcf13a7d73624adf16fc060d30e056
morikazu1119 <morikawakazuki11.19@gmail.com>
2025-07-23 13:52:56 +0900

    fix format

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
-from mcp.server.fastmcp import FastMCP
 import uvicorn
+from mcp.server.fastmcp import FastMCP
 
 mcp = FastMCP("recommendation-game-mcp")
+
 
 @mcp.tool()
 def recommend_game(request):
     return "おすすめはオセロです。"
+
 
 def main():
     uvicorn.run(
@@ -13,6 +15,7 @@ def main():
         host="0.0.0.0",
         port=8765,
     )
+
 
 if __name__ == "__main__":
     main()
